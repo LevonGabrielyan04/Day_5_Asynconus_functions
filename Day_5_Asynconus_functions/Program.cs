59cd50b6a70239db5602ac633994a0efa150c0c7
Levon Gabrielyan <levongabrielyan44@gmail.com>
2023-07-13 12:11:38 +0400

    Add project files.

---
+++ Day_5_Asynconus_functions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+
+class Task1
+{
+    string path;
+    public Task task;
+    public Task1(string Path)
+    {
+        task = new Task(() =>
+        {
+            string p = File.ReadAllText(this.path);
+            lock (Program.bigText)
+            {
+                Program.bigText += p + "\n";
+            }
+        });
+        path = Path;
+    }
+
+}
+class Task2
+{
+    string[] paths;
+    public Task2(string dir)
+    {
+        paths = Directory.GetFiles(dir);
+    }
+    public void Resize()
+    {
+        Bitmap bitmap = new Bitmap(paths[0]);
+    }
+}
+class Program
+{
+    public static string bigText = String.Empty;
+    static void Main()
+    {
+        //Task 1
+        //var a = new Task1(@"C:\A\someText.txt");
+        //var b = new Task1(@"C:\A\someText2.txt");
+        //a.task.Start();
+        //b.task.Start();
+        //Task.WaitAll(a.task, b.task);
+        //Console.WriteLine(Program.bigText);
+
+        //Task 2
+        Task2 task = new Task2(@"C:\B");
+    }
+}
